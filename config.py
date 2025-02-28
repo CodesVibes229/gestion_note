136687b46bba88fe64d4780b3f595dc029445514
pascal aurel <pascalaurel589@gmail.com>
2025-02-28 01:27:46 +0100

    Ajout de config.py au .gitignore et suppression du suivi Git

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'devsecretkey')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///notes.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
